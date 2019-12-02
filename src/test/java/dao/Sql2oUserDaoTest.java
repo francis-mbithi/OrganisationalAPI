0fc0251a031be62d3660cb773c7dd3b2756adf2e
francis-mbithi <frankmbithifm@gmail.com>
2019-12-02 09:14:14 +0300

    add methods to users and department

---
+++ src/test/java/dao/Sql2oUserDaoTest.java
@@ -0,0 +1,39 @@
+package dao;
+import models.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class Sql2oUserDaoTest {
+
+    private Connection conn;
+    private Sql2oUserDao userDao;
+
+    @Before
+    public void setup() throws Exception {
+        String connectionString = "jdbc:postgresql://localhost:5432/org_api_test";
+        Sql2o sql2o = new Sql2o(connectionString, "moringa", "c3l12i9f6f6");
+        userDao = new Sql2oUserDao(sql2o);
+        conn = sql2o.open();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+//        userDao.clearAll();
+        conn.close();
+    }
+
+    @Test
+    public void add() {
+        User user = setUpNewUser();
+        assertEquals(user.getId(), user.getId());
+    }
+}
